7f7acc753d88631c9bdd3e17687bcd1a8166ce86
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-06-01 20:03:31 +0800

    refactor: Let shared_context take params

--- spec/support/shared/api_client.rb
+++ spec/support/shared/api_client.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.shared_context 'API client', shared_context: :metadata do
+RSpec.shared_context 'API client', shared_context: :metadata do |params|
   before do
     OpenWeather::Config.reset
+    OpenWeather::Config.endpoint = params[:endpoint] if params&.key?(:endpoint)
   end
   let(:client) { OpenWeather::Client.new(api_key: ENV['OPEN_WEATHER_API_KEY'] || 'api-key') }
 end
