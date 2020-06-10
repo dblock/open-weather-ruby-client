6ed326a56cdc1aa6fe1b53bcf527471db5dd5321
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-06-10 22:26:47 +0800

    Use .any?

--- lib/open_weather/endpoints/stations.rb
+++ lib/open_weather/endpoints/stations.rb
@@ -38,7 +38,7 @@ module OpenWeather
       def get_measurements(options)
         required_keys = %i[station_id type limit from to]
         missing_keys = required_keys - options.keys
-        raise ArgumentError, "Missing params: #{missing_keys.join(', ')}" unless missing_keys.empty?
+        raise ArgumentError, "Missing params: #{missing_keys.join(', ')}" if missing_keys.any?
 
         get('measurements', options).map { |m| OpenWeather::Models::Stations::Measurement.new(m) }
       end
