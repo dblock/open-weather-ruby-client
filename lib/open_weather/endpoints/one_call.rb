3c2d2687019d0e76e27f7b3f51b354dced376a3b
Troy Anderson <troy@alliedcode.com>
2024-07-02 16:28:35 -0700

    Revert "Rubocop autocorrections"
    This reverts commit 6a044fc4a90fc7053ad7d788ad38e5ed9477a346.

--- lib/open_weather/endpoints/one_call.rb
+++ lib/open_weather/endpoints/one_call.rb
@@ -4,7 +4,7 @@ module OpenWeather
   module Endpoints
     module OneCall
       def one_call(lat, lon = nil, options = {})
-        options = lat.is_a?(Hash) ? options.merge(lat) : options.merge(lat:, lon:)
+        options = lat.is_a?(Hash) ? options.merge(lat) : options.merge(lat: lat, lon: lon)
         options[:exclude] = options[:exclude].join(',') if options[:exclude].is_a?(Array)
         options[:dt] = options[:dt].to_i if options[:dt].is_a?(Time)
         path = options.key?(:dt) ? '3.0/onecall/timemachine' : '3.0/onecall'
